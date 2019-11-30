887b32410c1becf85775aa58ac18ac5ee098a9ed
CarmanK <carmankevin97@gmail.com>
2019-11-30 18:04:08 -0500

    fixed a bug with a bad/simple  solution

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,11 @@ const LightState = v3.lightStates.LightState;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Hue Control Panel'});
+  res.render('index_on', {title: 'Hue Control Panel'});
 });
 
 /* POST home page. */
 router.post('/', function (req, res) {
-  res.render('index', {title: 'Hue Control Panel'});
   v3.discovery.nupnpSearch()
     .then(res => {
       const host = res[0].ipaddress;
@@ -26,18 +25,22 @@ router.post('/', function (req, res) {
             switch(req.body.brightness) {
               case -1: //Turn ON
                 state.transitionDefault().on()
+                res.render('index_on', {title: 'Hue Control Panel'});
                 break;
               case -2: //Turn OFF
                 state.transitionDefault().off()
+                res.render('index_off', {title: 'Hue Control Panel'});
                 break;
               default: // Adjust brightness
                 state.transitionDefault().on().brightness(req.body.brightness);
+                res.render('index_on', {title: 'Hue Control Panel'});
             }
             api.lights.setLightState(light._id, state);
           }
       });
     })
     .catch(e => {
+      res.render('switch_error', {error: e});
       console.log(`Error: ${e}`)
     });
 });
