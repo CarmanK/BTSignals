a056a474ba8cc5b52338b520fe928edf48f221e5
CarmanK <carmankevin97@gmail.com>
2019-11-30 13:49:33 -0500

    fixed up my sloppy code from late last night and added a render to prevent the post from timing out the webapp

--- routes/index.js
+++ routes/index.js
@@ -12,69 +12,34 @@ router.get('/', function(req, res, next) {
 
 /* POST home page. */
 router.post('/', function (req, res) {
-  switch(req.body.brightness) {
-    case -2: // Turn OFF
-      v3.discovery.nupnpSearch()
-      .then(res => {
-        const host = res[0].ipaddress;
-        return v3.api.createLocal(host).connect(user);
-      })
-      .then(api => {
-        api.lights.getAll()
-          .then(lights => {
-            for (var light of lights){ // Adjust each light connected to the hue bridge.
-              const state = new LightState()
-                .transitionDefault()
-                .off()
-              api.lights.setLightState(light._id, state);
+  res.render('index', {title: 'Hue Control Panel'});
+  v3.discovery.nupnpSearch()
+    .then(res => {
+      const host = res[0].ipaddress;
+      return v3.api.createLocal(host).connect(user);
+    })
+    .then(api => {
+      api.lights.getAll()
+        .then(lights => {
+          const state = new LightState()
+          for (var light of lights){ // Adjust each light connected to the hue bridge.
+            switch(req.body.brightness) {
+              case -1: //Turn ON
+                state.transitionDefault().on()
+                break;
+              case -2: //Turn OFF
+                state.transitionDefault().off()
+                break;
+              default: // Adjust brightness
+                state.transitionDefault().on().brightness(req.body.brightness);
             }
-        });
-      })
-      .catch(e => {
-        console.log(`Error: ${e}`)
+            api.lights.setLightState(light._id, state);
+          }
       });
-      break;
-    case -1: // Turn ON
-      v3.discovery.nupnpSearch()
-      .then(res => {
-        const host = res[0].ipaddress;
-        return v3.api.createLocal(host).connect(user);
-      })
-      .then(api => {
-        api.lights.getAll()
-          .then(lights => {
-            for (var light of lights){ // Adjust each light connected to the hue bridge.
-              const state = new LightState()
-                .transitionDefault()
-                .on()
-              api.lights.setLightState(light._id, state);
-            }
-        });
-      }).catch(e => {
-        console.log(`Error: ${e}`)
-      });
-      break;
-    default: // Adjust brightness
-      v3.discovery.nupnpSearch()
-      .then(res => {
-        const host = res[0].ipaddress;
-        return v3.api.createLocal(host).connect(user);
-      })
-      .then(api => {
-        api.lights.getAll()
-          .then(lights => {
-            for (var light of lights){ // Adjust each light connected to the hue bridge.
-              const state = new LightState()
-                .transitionDefault()
-                .on()
-                .brightness(req.body.brightness);
-              api.lights.setLightState(light._id, state);
-            }
-        });
-      }).catch(e => {
-        console.log(`Error: ${e}`)
-      });
-  }
+    })
+    .catch(e => {
+      console.log(`Error: ${e}`)
+    });
 });
 
 module.exports = router;
